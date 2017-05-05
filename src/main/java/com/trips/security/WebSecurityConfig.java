4c979b2727f7435822fc3eb70028ba477bff65a6
ndkmath1 <ndkmath1@gmail.com>
2017-05-05 18:21:57 +0700

    completed fix login

--- src/main/java/com/trips/security/WebSecurityConfig.java
+++ src/main/java/com/trips/security/WebSecurityConfig.java
@@ -13,7 +13,7 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
-//@Transactional
+// @Transactional
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
 	@Autowired
@@ -30,59 +30,28 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 	}
 
 	/**
-	 * Migrate from spring 3 to spring 4
-	 * attribute default value changed from /j_spring_security_check to POST /login
-	 * from /j_spring_security_logout to /logout
+	 * Migrate from spring 3 to spring 4 attribute default value changed from
+	 * /j_spring_security_check to POST /login from /j_spring_security_logout to
+	 * /logout
 	 */
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-        //Mixing http and https
+		// http.csrf().disable();
 		
-//		http.csrf().disable();
 		http.authorizeRequests().antMatchers("/", "/login").permitAll();
 		http.authorizeRequests().antMatchers("/admin/**").access("hasRole('ROLE_ADMIN')");
 		http.authorizeRequests().and().exceptionHandling().accessDeniedPage("/403");
-		http.authorizeRequests().and().formLogin()
-				.loginPage("/login")
-				.defaultSuccessUrl("/admin")
-				.failureUrl("/login?error=true")
-				.usernameParameter("username")
-				.passwordParameter("password")
-				.and().logout().logoutSuccessUrl("/");
+		http.authorizeRequests().and().formLogin().loginPage("/login").defaultSuccessUrl("/admin")
+				.failureUrl("/login?error=true").usernameParameter("username").passwordParameter("password").and()
+				.logout().logoutSuccessUrl("/");
 		
-//		http.requiresChannel().antMatchers("/login").requiresSecure();
-//		http.requiresChannel().anyRequest().requiresInsecure();
-//		http.requiresChannel().anyRequest().requiresSecure();
-
-		// http.authorizeRequests().antMatchers("/login",
-		// "/").permitAll().antMatchers("/admin/**").hasRole("ADMIN")
-		// //.anyRequest().authenticated()
-		// .and().exceptionHandling().accessDeniedPage("/403").and().formLogin()
-		// .loginPage("/login").usernameParameter("email").passwordParameter("password")
-		// .defaultSuccessUrl("/admin").failureUrl("/login?error");
-
-		// http.authorizeRequests().antMatchers("/", "/login").permitAll();
-		// http.authorizeRequests().antMatchers("/admin").access("hasRole('ROLE_ADMIN')");
-		// http.authorizeRequests().and().exceptionHandling().accessDeniedPage("/403");
-		// http.authorizeRequests().and().formLogin().loginPage("/login").usernameParameter("email")
-		// .passwordParameter("password").defaultSuccessUrl("/admin").failureUrl("/login?error");
-
-		// http
-		// .authorizeRequests()
-		// .antMatchers("/", "/login").permitAll()
-		// .antMatchers("/admin").hasRole("ADMIN")
-		// .and()
-		// .formLogin()
-		// .loginPage("/login")
-		// .usernameParameter("email")
-		// .passwordParameter("password")
-		// .defaultSuccessUrl("/admin")
-		// .failureUrl("/login?error")
-		// .and()
-		// .exceptionHandling()
-		// .accessDeniedPage("/403");
+		// Mixing http and https
+		// http.requiresChannel().antMatchers("/login").requiresSecure();
+		// http.requiresChannel().anyRequest().requiresInsecure();
+		// http.requiresChannel().anyRequest().requiresSecure();
 
 		System.out.println("## set role for admin success");
+
 	}
 
 }
